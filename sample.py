014812c6fee79a07dbec19337f2f80daa2daf929
matigamez <matisanchezg95@gmail.com>
2024-07-12 17:59:57 -0400

    error corregido

--- sample.py
+++ sample.py
@@ -8,21 +8,24 @@ def ingresar_comentarios():
     while True:
         print('Por favor, introduzca la evaluación entre 1 y 5')  
         point = input()
-        if point.isdecimal():
-            point = int(point)
+        while True:
+            if point.isdecimal():
+                point = int(point)
+            else:
+                print('Por favor, introduzca los puntos de evaluación como un número') 
             if point <= 0 or point > 5:  # Condición: menor o igual a 0 o mayor que 5
                 print('Por favor, introduzca un número entre 1 y 5')  
                 point = input()
             else:
-                print('Por favor, introduzca su comentario')  
-                comment = input()
-                post = f'Puntos: {point} Comentarios: {comment}'  
-                file_pc = open("data.txt", 'a')
-                file_pc.write(f'{post} \n')
-                file_pc.close()
                 break
-        else:
-            print('Por favor, introduzca los puntos de evaluación como un número') 
+        
+        print('Por favor, introduzca su comentario')  
+        comment = input()
+        post = f'Puntos: {point} Comentarios: {comment}'  
+        file_pc = open("data.txt", 'a')
+        file_pc.write(f'{post} \n')
+        file_pc.close()
+        break
 
 def imprimir_menu():
     print('Por favor, seleccione la operación que desea realizar')  
